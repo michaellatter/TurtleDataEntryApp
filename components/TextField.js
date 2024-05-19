942a99e38948250a27f748660c1603d13032307d
mlatter3 <mlatter3@myune.edu.au>
2024-05-19 14:21:18 +1000

    Created TextField.js

---
+++ components/TextField.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { useTheme } from '../context/ThemeContext';
+
+const TextField = ({ label, value, onChangeText }) => {
+  const { theme } = useTheme();
+
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.label, { color: theme.text }]}>{label}</Text>
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        style={[styles.input, { color: theme.text, backgroundColor: theme.rectangle }]}
+      />
+    </View>
+  );
+};
+
+export default TextField;
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+  label: {
+    marginBottom: 5,
+    fontSize: 16,
+  },
+  input: {
+    height: 40,
+    width: 150,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+  },
+});
